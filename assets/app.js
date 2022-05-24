c02fb48c8c5db9fd0d83fe564b3e28a0e0557478
alainaissi <89066710+alainaissi@users.noreply.github.com>
2022-05-24 12:32:28 +0200

    quest4

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../public/build/app.css';
 
 // start the Stimulus application
 import './bootstrap';
